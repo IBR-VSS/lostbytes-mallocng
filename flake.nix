e373b84f5c6be94d9e049164c5ecdea7c1612e6f
Pasha Fistanto <pasha@fstn.top>
2026-04-01 20:42:10 +0200

    add flake support

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Mallocng development flake";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            packages = [
+              clang-tools
+              bear
+            ];
+          };
+      }
+    );
+}
