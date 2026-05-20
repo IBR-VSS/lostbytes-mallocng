8fc509175867adf786af1f5d2a11c317722dc47d
Pasha Fistanto <fistanto@ibr.cs.tu-bs.de>
2026-05-20 14:32:27 +0200

    add nomerge

--- flake.nix
+++ flake.nix
@@ -5,20 +5,16 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-    }:
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default =
-          with pkgs;
+      in rec {
+        packages.default = with pkgs;
           stdenv.mkDerivation {
             name = "mallocng-lit";
             src = ./.;
@@ -30,14 +26,21 @@
               cp libmallocng.so $out
             '';
           };
-        devShell =
-          with pkgs;
+        packages.noMerge = packages.default.overrideAttrs (oldAttrs: {
+          patches =
+            (oldAttrs.patches or [])
+            ++ [
+              ./no_merge.patch
+            ];
+        });
+        devShell = with pkgs;
           mkShell {
             packages = [
               clang-tools
+
               bear
             ];
-            inputsFrom = [ self.packages.${system}.default ];
+            inputsFrom = [self.packages.${system}.default];
           };
       }
     );
